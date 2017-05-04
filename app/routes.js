71e09cce2601b7c7a0d33984b3c9f1d3673025b7
Pasan Missaka <psn.missaka@gmail.com>
2017-05-04 07:23:15 +0530

    Added the bower to manager the front end dependencies

--- app/routes.js
+++ app/routes.js
@@ -24,7 +24,7 @@ module.exports = function(app){
 	})
 
 	app.get('*', function(req, res) {
-		res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+		res.sendfile('./public/index.html'); 
 	});
 
 }
