55b5610773edc4151a94949a87e2c708f74b9775
Pasan Missaka <psn.missaka@gmail.com>
2017-05-21 13:27:15 +0530

    changed to the port to 8080

--- stackle_api/server.js
+++ stackle_api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-var port     = process.env.PORT || 3000;
+var port     = process.env.PORT || 8080;
 var mongoose = require('mongoose'); 
 var database = require('./config/database');            // load the database config
 var morgan = require('morgan');             // log requests to the console (express4)
