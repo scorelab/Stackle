8ece17cd0989749d1408a385b06e54b435f4157c
Pasan Missaka <psn.missaka@gmail.com>
2017-06-04 21:08:45 +0530

    configuring the app.js

--- stackle_app/app/scripts/app.js
+++ stackle_app/app/scripts/app.js
@@ -15,9 +15,47 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ngMaterial',
+    'auth0',
+    'angular-storage',
+    'angular-jwt',
+    'ui.router'
   ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider,
+                    $provide,
+                    authProvider,
+                    $urlRouterProvider,
+                    $stateProvider,
+                    $httpProvider,
+                    jwtInterceptorProvider,
+                    $mdIconProvider) {
+
+    authProvider.init({
+      domain : 'psnmissaka.au.auth0.com',
+      clientID : 'f6ewsNvcnhe8ODntv_FAD_lW_YPvxN5X'
+    });
+
+    $mdIconProvider
+           .iconSet('social', 'img/icons/sets/social-icons.svg', 24)
+           .defaultIconSet('img/icons/sets/core-icons.svg', 24);
+
+    $stateProvider
+      .state('home',{
+        url : '/home',
+        templateUrl : 'app/components/home/home.html'
+      })
+      .state('profile',{
+        url :'/profile',
+        templateUrl : 'app/components/profile/profile.html',
+        controller : 'profileController as user'
+      })
+      .state('landing',{
+        url : '/landing',
+        templateUrl : 'app/components/landing/landing.html',
+        controller : 'landingController as home'
+      });
+
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
@@ -27,4 +65,8 @@ angular
       .otherwise({
         redirectTo: '/'
       });
+
+
+
+
   });
