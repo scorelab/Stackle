8bbfb3fdaec0f46f58b145d86fdd805ddfaa857b
Pasan Missaka <psn.missaka@gmail.com>
2017-06-04 21:25:25 +0530

    Added the basic controllers and respectives views for them

---
+++ stackle_app/app/scripts/controllers/profile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('stackleAppApp')
+  .controller('profileController', function ($http) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
