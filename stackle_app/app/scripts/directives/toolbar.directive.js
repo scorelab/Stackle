911d8d858c56b8de2e07da9a75c96204bfee94a2
Pasan Missaka <psn.missaka@gmail.com>
2017-06-22 22:30:21 +0530

    Added the scripts, bower components and node modules

---
+++ stackle_app/app/scripts/directives/toolbar.directive.js
@@ -0,0 +1,39 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('stackleApp')
+        .directive('toolbar',toolbar);
+
+        function toolbar(){
+            return {
+                templateUrl: 'app/components/toolbar/toolbar.html',
+                controller : toolbarController,
+                controllerAs: 'toolbar'
+            }
+        }
+
+        function toolbarController(auth, store, $location){
+            var vm = this;
+            vm.login = login;
+            vm.logout = logout;
+            vm.auth = auth;
+
+            function login(){
+                auth.signin({},function(profile, token){
+                    store.set('profile', profile);
+                    store.set('id_token', token);
+                    $location.path('/landing');
+                }, function(err){
+                    console.log(err);
+                });
+            }
+
+            function logout(){
+                store.remove('profile');
+                store.remove('id_token');
+                auth.signout();
+                $location.path('/home');
+            }
+        }
+})();
