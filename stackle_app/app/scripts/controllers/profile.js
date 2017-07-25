eb425b7a3929e5fbd2378694f601eb9589ea5292
Pasan Missaka <psn.missaka@gmail.com>
2017-07-25 16:16:10 +0530

    Added profile page

--- stackle_app/app/scripts/controllers/profile.js
+++ stackle_app/app/scripts/controllers/profile.js
@@ -1,11 +1,13 @@
 'use strict';
 
 angular.module('stackleAppApp')
-  .controller('profileController', function ($http) {
+  .controller('profileController', function ($http, $scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.profile = JSON.parse(localStorage.getItem("profile"));
 
   });
