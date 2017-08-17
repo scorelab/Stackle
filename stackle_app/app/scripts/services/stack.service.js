719dee6585a7a1e35ded422daf1c10016df479a7
Pasan Missaka <psn.missaka@gmail.com>
2017-08-17 08:04:51 +0530

    Added post service

--- stackle_app/app/scripts/services/stack.service.js
+++ stackle_app/app/scripts/services/stack.service.js
@@ -1,44 +1,49 @@
-(function(){
+(function () {
   'use strict';
 
   angular.module('stackleAppApp')
-    .service('stackService',['$http', stackService]);
+    .service('stackService', ['$http', stackService]);
 
-    function stackService($http){
+  function stackService($http) {
 
-      var searchStack = function(name){
-        var url = "https://api.github.com/orgs/"+name;
+    var searchStack = function (name) {
+      if (name) {
+        var url = "https://api.github.com/orgs/" + name;
 
         console.log("Searching org :" + name);
 
-        $http.get(url).then(function(response){
-          return response.data;
-        }, function error(response){
+        $http.get(url).then(function success(response) {
+          return response;
+        }, function error(response) {
           return "error occured";
         })
+      }else{
+        return "Can't get!";
       }
-
-      var createStack = function(name, username){
-        var url = "https://api.github.com/orgs/{{name}}/members";
-
-        $http.get(url).then(function(response){
-          var members = JSON.parse(response.data);
-          for(var member in members){
-            if(member.login == name){
-              return "you are a member";
-            }else{
-              return "you are not a member";
-            }
-          }
-        }, function error(response){
-          return "error occured!"
-        })
-      }
-
-      return {
-        searchStack : searchStack,
-        createStack : createStack
-      }
-
+      
     }
+
+    var createStack = function (name, username) {
+      var url = "https://api.github.com/orgs/{{name}}/members";
+
+      $http.get(url).then(function (response) {
+        var members = JSON.parse(response.data);
+        for (var member in members) {
+          if (member.login == name) {
+            return "you are a member";
+          } else {
+            return "you are not a member";
+          }
+        }
+      }, function error(response) {
+        return "error occured!"
+      })
+    }
+
+    return {
+      searchStack: searchStack,
+      createStack: createStack
+    }
+
+  }
 })();
--- stackle_app/app/scripts/services/user.service.js
+++ stackle_app/app/scripts/services/user.service.js
@@ -1,24 +1,25 @@
-(function(){
+(function () {
   'use strict';
 
   angular.module('stackleAppApp')
-    .service('userService',['$http', userService]);
+    .service('userService', ['$http', userService]);
 
-    function userService($http){
+  function userService($http) {
 
-      var getOrgs = function(){
-        var url = JSON.parse(localStorage.getItem("profile")).organizations_url;
-        console.log("orgs url is " + url)
+    var getOrgs = function () {
+      var url = JSON.parse(localStorage.getItem("profile")).organizations_url;
+      console.log("orgs url is " + url)
 
-        $http.get(url).then(function(response){
-          return response.data;
-        }, function error(response){
-          return "error_occured";
-        })
-      }
-
-      return {
-        getOrgs : getOrgs
-      }
+      $http.get(url).then(function (response) {
+        console.log(response.data);
+        return response;
+      }, function error(response) {
+        return "error_occured";
+      })
     }
+
+    return {
+      getOrgs: getOrgs
+    }
+  }
 })();
