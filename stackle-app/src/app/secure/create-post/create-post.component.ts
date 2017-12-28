ca8d622be466b6dbcacddc6f9c9d114789fe9d60
Pasan Missaka <psn.missaka@gmail.com>
2017-12-29 01:59:06 +0530

    created the create post component and the route to it

---
+++ stackle-app/src/app/secure/create-post/create-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-post',
+  templateUrl: './create-post.component.html',
+  styleUrls: ['./create-post.component.css']
+})
+export class CreatePostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
