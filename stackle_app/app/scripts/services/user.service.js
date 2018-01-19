ec62116abf9ad34754c78c4b4a85f45fcabf6151
Adhyan Dhull <dhulladhyan2001@gmail.com>
2018-01-19 13:18:09 +0530

    Update user.service.js
    Fixed : 
    app\scripts\services\user.service.js
      line 15  col 9   Missing semicolon.
      line 16  col 6   Missing semicolon.
      line 22  col 9   Missing semicolon.
      line 28  col 6   Missing semicolon.
    
      ‼  4 warnings

--- stackle_app/app/scripts/services/user.service.js
+++ stackle_app/app/scripts/services/user.service.js
@@ -12,19 +12,19 @@
         return response;
       }, function error(response) {
         return "error_occured";
-      })
-    }
+      });
+    };
 
     var getSubscribedStacks = function(userid, callback){
       var apiUrl = server + 'api/stack/subscribed/'+ userid;
       $http.get(apiUrl).then(function(response){
         return callback(response.data);
-      })
+      });
     };
 
     return {
       getOrgs: getOrgs,
       getSubscribedStacks : getSubscribedStacks
-    }
+    };
   }
 })();
