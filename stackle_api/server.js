f884626f5540d7db5b3519eb795b56f3f47d2caa
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-02-16 21:42:17 +0530

    Update PR to improve Codacy Report
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- stackle_api/server.js
+++ stackle_api/server.js
@@ -8,15 +8,15 @@ var bodyParser = require('body-parser');    // pull information from HTML POST (
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 var db = mongoose.connection;
 
-app.use('/', express.static(__dirname +  '/'));
+app.use('/', express.static(__dirname + '/'));
 app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({'extended': 'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
 // No need to store it into a variable
-require('./app/routes')(app,db);
+require("./app/routes")(app, db);
 
 app.use(function (err, req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:8082");
@@ -26,16 +26,16 @@ app.use(function (err, req, res, next) {
   res.status(500).send('Something broke!')
 });
 
-mongoose.connect(database.url,function(err){
-    console.log("Connecting to the database..");
-    if(err){
-        mongoose.connect(database.alturl,function(err){
-            return console.log(err);
-        })
-        return console.log("Couldnt connect to db url 1. connecting to alternate");
-    }else{
-        console.log("Mongo connect sucess!");
-    }
+mongoose.connect(database.url, function (err) {
+  console.log("Connecting to the database..");
+  if (err) {
+    mongoose.connect(database.alturl, function (err) {
+      return console.log(err);
+    })
+    return console.log("Couldnt connect to db url 1. connecting to alternate");
+  } else {
+    console.log("Mongo connect sucess!");
+  }
 });     // connect to mongoDB database on modulus.io
 
 module.exports = app;
