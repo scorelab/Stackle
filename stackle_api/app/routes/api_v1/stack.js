c81df77fdb06fb757494a65e558e1928cfd9f6d2
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-02-18 19:27:52 +0530

    API routes for stack
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

---
+++ stackle_api/app/routes/api_v1/stack.js
@@ -0,0 +1,44 @@
+/**
+ * Created by bhavyaagg on 18/02/18.
+ */
+
+const express = require('express');
+const router = express.Router();
+
+const Stack = require('../../models/stack');
+const User = require('../../models/user');
+
+//create stack
+router.post('/create', function (req, res) {
+    let stack = new Stack(req.body);
+    stack.save(function (err, stack) {
+        if (err) {
+            console.log("Error saving the stack to database");
+            res.send("Error saving stack!");
+        } else if (stack) {
+            res.send("Sucessfully created the stack");
+        } else {
+            res.send("Null");
+        }
+    })
+})
+
+//getting subscribed stacks for a user
+router.get('/subscribed/:userid', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    User.findOne({userId: req.params.userid}, function (err, result) {
+        if (err) {
+            res.send(err);
+        } else if (result) {
+            let sub_stack = result.subscribed_stacks;
+            res.send(sub_stack);
+        } else {
+            res.send("Can't get!");
+        }
+    })
+})
+
+
+exports = module.exports = router;
