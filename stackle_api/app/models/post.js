e1dce9772961760ca2d650cf92770ef2be6993f9
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-02-19 22:37:26 +0530

    Update models to solve codacy issues
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- stackle_api/app/models/post.js
+++ stackle_api/app/models/post.js
@@ -1,30 +1,30 @@
 var mongoose = require('mongoose');
 
 var replySchema = mongoose.Schema({
-	description : String,
-	user : String,
-	date : String
+    description: String,
+    user: String,
+    date: String
 });
 
 var commentSchema = mongoose.Schema({
-	description : String,
-	user : String,
-	votes : Number,
-	date : String,
-	replies : [replySchema]
+    description: String,
+    user: String,
+    votes: Number,
+    date: String,
+    replies: [replySchema]
 });
 
 var postSchema = mongoose.Schema({
-	title : String,
-	description : String,
-	org_name : String,
-	tags : [],
-	repository : String,
-	link_issue : String,
-	user : String,
-	date : String,
-	votes : Number,
-	comments : [commentSchema]
+    title: String,
+    description: String,
+    org_name: String,
+    tags: [],
+    repository: String,
+    link_issue: String,
+    user: String,
+    date: String,
+    votes: Number,
+    comments: [commentSchema]
 });
 
 var Post = mongoose.model('Post', postSchema);
@@ -32,7 +32,7 @@ var Comment = mongoose.model('Comment', commentSchema);
 var Reply = mongoose.model('Reply', replySchema);
 
 module.exports = {
-	Post : Post,
-	Comment : Comment,
-	Reply : Reply
+    Post: Post,
+    Comment: Comment,
+    Reply: Reply
 }
--- stackle_api/app/models/user.js
+++ stackle_api/app/models/user.js
@@ -1,20 +1,20 @@
 var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
-	userId : String,
-	github : {
-		id : String,
-		token : String,
-		email : String,
-		name : String
-	},
-	gitlab : {
-		id : String,
-		token : String,
-		email : String,
-		name : String
-	},
-	subscribed_stacks : []
+    userId: String,
+    github: {
+        id: String,
+        token: String,
+        email: String,
+        name: String
+    },
+    gitlab: {
+        id: String,
+        token: String,
+        email: String,
+        name: String
+    },
+    subscribed_stacks: []
 });
 
 module.exports = mongoose.model('User', userSchema);
