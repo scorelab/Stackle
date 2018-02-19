4a5415473edd4baa9e79e02f6929a8b896a461a0
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-02-19 22:36:47 +0530

    Add .eslintrc.js
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    root: true,
+    parser: 'babel-eslint',
+    parserOptions: {
+        sourceType: 'module'
+    },
+    extends: [
+        'eslint:recommended',
+        'standard'
+    ],
+    rules: {
+
+    }
+}
