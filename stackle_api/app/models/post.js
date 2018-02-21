1a49dc2b24b9a6e7541b2c0f3f194f64f4264e24
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-02-21 11:05:04 +0530

    Use const in Post model, Properly indent post model file, Use camel case for keys
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- stackle_api/app/models/post.js
+++ stackle_api/app/models/post.js
@@ -1,38 +1,38 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var replySchema = mongoose.Schema({
-	description : String,
-	user : String,
-	date : String
+const replySchema = mongoose.Schema({
+    description: String,
+    user: String,
+    date: String
 });
 
-var commentSchema = mongoose.Schema({
-	description : String,
-	user : String,
-	votes : Number,
-	date : String,
-	replies : [replySchema]
+const commentSchema = mongoose.Schema({
+    description: String,
+    user: String,
+    votes: Number,
+    date: String,
+    replies: [replySchema]
 });
 
-var postSchema = mongoose.Schema({
-	title : String,
-	description : String,
-	org_name : String,
-	tags : [],
-	repository : String,
-	link_issue : String,
-	user : String,
-	date : String,
-	votes : Number,
-	comments : [commentSchema]
+const postSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    org_name: String,
+    tags: [],
+    repository: String,
+    linkIssue: String,
+    user: String,
+    date: String,
+    votes: Number,
+    comments: [commentSchema]
 });
 
-var Post = mongoose.model('Post', postSchema);
-var Comment = mongoose.model('Comment', commentSchema);
-var Reply = mongoose.model('Reply', replySchema);
+const Post = mongoose.model('Post', postSchema);
+const Comment = mongoose.model('Comment', commentSchema);
+const Reply = mongoose.model('Reply', replySchema);
 
 module.exports = {
-	Post : Post,
-	Comment : Comment,
-	Reply : Reply
+    Post,
+    Comment,
+    Reply
 }
