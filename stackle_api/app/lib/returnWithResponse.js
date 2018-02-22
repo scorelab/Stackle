393c2452bc962b8ada74a3a031a8f259f34f6b2b
AndrewJR350 <andrewjr350@gmail.com>
2018-02-22 11:34:13 +0530

    Modularized the response configuration

---
+++ stackle_api/app/lib/returnWithResponse.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * Pass the response object and data you want to throw.
+ * For an example 
+ * {
+ *  status: 200 - required
+ *  result: any - required
+ * }
+ * @param { Object } data status
+ * @param { Object } response response
+ */
+function configureReturnData(data, response) {
+  if (!!!~Object.keys(data).indexOf('status')) {
+    throw new Error('Attribute status is missing');
+  }
+  
+  if (!!!~Object.keys(data).indexOf('result')) {
+    throw new Error('Attribute result is missing');
+  }
+
+  response.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.status(data.status);
+  response.send({
+    status: data.status,
+    result: data.result,
+  });
+};
+
+module.exports.configureReturnData = configureReturnData;
--- stackle_api/app/routes.js
+++ stackle_api/app/routes.js
@@ -1,206 +1,241 @@
-// application -------------------------------------------------------------
+/** -------------------------  application -------------------------------- */
+'use strict';
+const mongoose = require('mongoose');
+
 const User = require('./models/user');
 const Stack = require('./models/stack');
-
-const postModels = require('./models/post')
+const postModels = require('./models/post');
+const returnWithResponse = require('./lib/returnWithResponse');
 
 const Post = postModels.Post;
 const Comment = postModels.Comment;
+const Reply = postModels.Reply;
 
 module.exports = function (app, db) {
 
 	//api
-	app.get('/api/login/', function (req, res) {
+	app.get('/api/login/', function (request, response) {
 	})
 
-	app.get('/home', function (req, res) {
+	app.get('/home', function (request, response) {
 		//needs to intergrate with github for implementation
-		res.end();
+		response.end();
 	})
 
 	//get all posts
-	app.get('/api/posts', function (req, res) {
-		res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-		Post.find({}, function (err, posts) {
-			if (err)
-				console.log("Cant get all posts!")
-			res.status(200).send(posts);
-		})
-	})
-
-	//save a post
-	app.post('/api/user/post', function (req, res) {
-		let post = new Post(req.body);
-		post.save(function (err, post) {
-			if (err) {
-				console.log("error saving the post");
-				res.status(500).send("error!")
-			} else {
-				res.status(201).send("Sucessfully saved the post!");
+	app.get('/api/posts', function (request, response) {
+		Post.find({}, (error, postsDetails) => {
+			if (error) {
+				return returnWithResponse.configureReturnData({
+					status: 400,
+					result: error
+				}, response);
 			}
+
+			return returnWithResponse.configureReturnData({
+				status: 200,
+				result: postsDetails,
+			}, response);
+		});
+	});
+	'use strict';
+
+	/**
+	 * pass the response object and data you want to throw
+	 * Example 
+	 * {
+	 *  status: 200 - required
+	 *  result: any - required
+	 * }
+	 * @param { Object } data status
+	 * @param { Object } response response
+	 */
+	function configureReturnData(data, response) {
+		if (!!!~Object.keys(data).indexOf('status')) {
+			throw new Error('Attribute status is missing');
+		}
+		
+		if (!!!~Object.keys(data).indexOf('result')) {
+			throw new Error('Attribute result is missing');
+		}
+	
+		response.header("Access-Control-Allow-Origin", "*");
+		res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+		response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		response.status(data.status);
+		response.send({
+			status: data.status,
+			result: data.result,
+		});
+	};
+	
+	module.exports.configureReturnData = configureReturnData;
+	//save a post
+	app.post('/api/user/post', function (request, response) {
+		const post = new Post(request.body);
+		post.save((error, insertedPost) => {
+			if (error) {
+				return returnWithResponse.configureReturnData({ status: 400, result: error }, response);
+			}
+
+			return returnWithResponse.configureReturnData({ status: 200, result: insertedPost._id }, response);
 		});
 	})
 
 	//get a post by id
-	app.get('/api/post/:postid', function (req, res) {
-		res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-		let objectid = req.params.postid;
-		Post.findOne({ _id: objectid }, function (err, post) {
-			if (err) {
-				res.status(404).send(err);
-			} else {
-				res.status(200).send(post);
+	app.get('/api/post/:postid', function (request, response) {
+		const  postId = request.params.postid;
+		Post.findOne({ _id: postId }, (error, postDetails) => {
+			if (error) {
+				return returnWithResponse.configureReturnData({ status: 400, result: error }, response);
 			}
-		})
-	})
 
-	//delete a post by ID
-	app.delete('/api/post/:postid', function (req, res) {
-		let postid = req.params.postid;
-		Post.remove({ _id: postid }, function (err, success) {
-			if (err) {
-				console.log(err);
-				res.status(500).send("Error deleting the document");
-			} else if (success) {
-				res.status(200).send("Sucessfully Deleted");
-			} else {
-				console.log("Null pointer");
-			}
-		})
-	})
-
-	//returns posts by a specific user
-	app.get('/api/posts/:user', function (req, res) {
-		let id = req.params.user;
-		Post.find({ user: id }, function (err, posts) {
-			if (err)
-				console.log("Erorr getting posts");
-			res.status(200).send(posts);
-		})
-	})
-
-	//returns posts relating to specific org
-	app.get('/api/posts/org/:org_name', function (req, res) {
-		res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-		let orgname = req.params.org_name;
-		Post.find({ org_name: orgname }, function (err, posts) {
-			if (err)
-				console.log(`Error getting posts from $orgname`);
-			else
-				res.status(200).send(posts);
-		})
-	})
-
-	//get a specific org
-	app.get('/api/org/:orgname', function (req, res) {
-		res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-		let orgname = req.params.orgname;
-		Stack.find({ name: orgname }, function (err, org) {
-			if (err) {
-				console.log('Error');
-			} else {
-				res.status(200).send(org);
-			}
-		})
-	})
-
-	//comment on a post
-	app.post('/api/comment/:postid', function (req, res) {
-		let postid = req.params.postid;
-		let query = { _id: postid };
-		let comment = new Comment(req.body);
-		Post.update(query, { comments: [] });
-	})
-
-	//get all stacks (orgs)
-	app.get('/api/orgs', function (req, res) {
-		Stack.find({}, function (err, stacks) {
-			if (err)
-				console.log("Errors retrieving stacks!");
-			else
-				res.status(200).send(stacks);
-		})
-	})
-
-	//create stack
-	app.post('/api/stack/create', function (req, res) {
-		let stack = new Stack(req.body);
-		stack.save(function (err, stack) {
-			if (err) {
-				console.log("Error saving the stack to database");
-				res.status(500).send("Error saving stack!");
-			} else if (stack) {
-				res.status(201).send("Sucessfully created the stack");
-			} else {
-				res.status(500).send("Null");
-			}
-		})
-	})
-
-	//delete stack
-	app.delete('api/delete/stack/:stackid', function (req, res) {
-		let stack_id = req.params.stackid;
-		Stack.remove({ _id: stack_id }, function (err, success) {
-			if (err) {
-				res.status(500).send("Couldn't delete Stack");
-			} else {
-				res.status(200).send("");
-			}
-		})
-	})
-
-	//user subscribing to an stack
-	app.post('/api/subscribe', function (req, res) {
-		let userid = req.body.uid;
-		let stackname = req.body.stack_name;
-		let query = { userId : userid };
-		User.findOneAndUpdate(query, {$push: {subscribed_stacks : stackname}}, function(err, noaffected){
-			if(err){
-				res.status(500).send("Error Updating");
-			}else{
-				res.status(200).send("Success!!");
-			}
+			return returnWithResponse.configureReturnData({ status: 200, result: postDetails }, response);
 		});
 	})
 
-	//getting subscribed stacks for a user
-	app.get('/api/stack/subscribed/:userid', function(req ,res){
-		res.header("Access-Control-Allow-Origin", "*");
-		res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-		User.findOne({userId : req.params.userid}, function(err, result){
-			if(err){
-				res.status(500).send(err);
-			}else if(result){
-				let sub_stack = result.subscribed_stacks;
-				res.status(200).send(sub_stack);
-			}else{
-				res.status(500).send("Can't get!");
+	//delete a post by ID
+	app.delete('/api/post/:postid', function (request, response) {
+		const postId = request.params.postid;
+		Post.remove({ _id: postId }, (error, result) => {
+			if (error) {
+				return returnWithResponse.configureReturnData({ status: 400, result: error }, response);
 			}
-		})
+			
+		return returnWithResponse.configureReturnData({ status: 200, result: `${postId} was sucessfully deleted`}, response);
+		});
+	});
+
+	//returns posts by a specific user
+	app.get('/api/posts/:user', function (request, response) {
+		const id = request.params.user;
+		Post.find({ user: id }, (error, userPosts) => {
+			if (error) {
+				return returnWithResponse.configureReturnData({ status: 400, result: error }, response);
+			}
+
+			return returnWithResponse.configureReturnData({ status: 200, result: userPosts }, response);
+		});
+	});
+
+	//returns posts relating to specific organisation
+	app.get('/api/posts/org/:org_name', function (request, response) {
+		const organisationName = request.params.org_name;
+		Post.find({ org_name: organisationName }, (error, organisationPosts) => {
+			if (error) {
+				return returnWithResponse.configureReturnData({ status: 400, result: error }, response);
+			}
+
+			return returnWithResponse.configureReturnData({ status: 200, result: organisationPosts }, response);
+		});
+	});
+
+	// get a specific organisation
+	app.get('/api/org/:orgname', function (request, response) {
+		const organisationName = request.params.orgname;
+		Stack.find({ name: organisationName }, (error, organisationDetails) => {
+			if (error) {
+				return returnWithResponse.configureReturnData({ status: 400, result: error }, response);
+			}
+
+			return returnWithResponse.configureReturnData({ status: 200, result: organisationDetails }, response);
+		});
 	})
 
-	//create user
-	app.post('/api/newuser', function (req, res) {
-		let user = new User(req.body);
-		user.save(function (err, user) {
-			if (err) {
-				console.log("Error saving the stack to database");
-				res.status(500).send("Error saving user!");
-			} else {
-				res.status(201).send("Sucessfully created the user");
+	// comment on a post
+	app.post('/api/comment/:postid', function (request, response) {
+		const query = {
+			_id: request.params.postid,
+		};
+		const comment = new Comment(request.body);
+		Post.update(query, { comments: [] }, (error, result) => {
+			if (error) {
+				return returnWithResponse.configureReturnData({ status: 400, result: error }, response);
 			}
-		})
-	})
 
-	app.get('/api/notifications', function (req, res) { });
+			return returnWithResponse.configureReturnData({ status: 200, result: `${ request.params.postid} successfully updated`},
+			response);
+		});
+	});
 
-	app.get('/*', function (req, res) {
-		res.sendfile('./public/404.html');
+	// get all stacks (organisation)
+	app.get('/api/orgs', function (request, response) {
+		Stack.find({}, (error, stacksDetails) => {
+			if (error) {
+				return returnWithResponse.configureReturnData({ status: 400, result: error }, response);
+			}
+
+			return returnWithResponse.configureReturnData({ status: 200, result: stacksDetails }, response);
+		});
+	});
+
+	// create stack
+	app.post('/api/stack/create', function (request, response) {
+		const stack = new Stack(request.body);
+		stack.save((error, insertedStack) => {
+			if (error) {
+				return returnWithResponse.configureReturnData({ status: 400, result: error }, response);
+			}
+			
+			return returnWithResponse.configureReturnData({ status: 200, result: insertedStack._id }, response);
+		});
+	});
+
+	// delete stack
+	app.delete('api/delete/stack/:stackid', function (request, response) {
+		const stackId = request.params.stackid;
+		Stack.remove({ _id: stackId }, (error, result) => {
+			if (error) {
+				return returnWithResponse.configureReturnData({ status: 400, result: error }, response);
+			}
+			return returnWithResponse.configureReturnData({ status: 200, result: `${stackId} was sucessfully deleted` },
+			response);
+		});
+	});
+
+	// user subscribing to an stack
+	app.post('/api/subscribe', function (request, response) {
+		const stackName = request.body.stack_name;
+		const findQuery = {
+			userId: request.body.uid,
+		};
+		User.findOneAndUpdate(findQuery, { $push: { subscribed_stacks : stackName } }, (err, noaffected) => {
+			if (error) {
+				return returnWithResponse.configureReturnData({ status: 400, result: error }, response);
+			}
+			
+			return returnWithResponse.configureReturnData({ status: 200,result: `${stackId} was sucessfully deleted`},
+			response);
+		});
+	});
+
+	// getting subscribed stacks for a user
+	app.get('/api/stack/subscribed/:userid', function(request ,response) {
+		User.findOne({ userId : request.params.userid }, (error, userDetails) => {
+			if (error) {
+				return returnWithResponse.configureReturnData({ status: 400, result: error }, response);
+			}
+			
+			return returnWithResponse.configureReturnData({ status: 200, result: userDetails.subscribed_stacks }, response);
+		});
+	});
+
+	// create user and retruning created user id
+	app.post('/api/newuser', function (request, response) {
+		const user = new User(request.body);
+		user.save(function (error, insertedUser) {
+			if (error) {
+				return returnWithResponse.configureReturnData({ status: 400, result: error }, response);
+			}
+			
+			return returnWithResponse.configureReturnData({ status: 200, result: insertedUser._id }, response);
+		});
+	});
+
+	app.get('/api/notifications', function (request, response) { });
+
+	app.get('/*', function (request, response) {
+		response.sendfile('./public/404.html');
 	});
 
 }
