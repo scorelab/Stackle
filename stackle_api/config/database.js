a81bc20338cb07863aba11b962de1b167e85264a
Shivam Arora <shivam_arora@live.in>
2018-02-26 01:49:30 +0530

    Fix Warning  Issue #97
    Fix warning for mongoose connection

--- stackle_api/config/database.js
+++ stackle_api/config/database.js
@@ -1,5 +1,12 @@
 module.exports = {
-	url : process.env.LOCAL_DB,
+	url : process.env.LOCAL_DB || 'mongodb://localhost',
 	alturl : 'mongodb://ordinary:H3ll0w0rld@ds149201.mlab.com:49201/stackle',
-
 };
+
+/*
+
+To start local mongodb server use following command:
+
+sudo service mongod start
+
+*/
