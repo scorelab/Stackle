8c4a362fe46d7d3de5a09038b8b1fb091995c2bb
Nihal Patel <nihalmpatel@gmail.com>
2018-03-12 17:10:39 +0530

    Added mongoose validation.

--- stackle_api/app/models/stack.js
+++ stackle_api/app/models/stack.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const stackSchema = mongoose.Schema({
-    name: String,
-    description: String,
-    stackleUrl: String,
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    stackleUrl: { type: String, required: true },
     githubUrl: String,
-    createdUser: String
+    createdUser: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Stack', stackSchema);
