e77e2b5fb4fb2b521a594e249a6d9a1477dbb824
Shivam Arora <shivam_arora@live.in>
2018-07-04 20:28:29 +0530

    PassportJS file Well Formated

--- stackle_api/app/lib/passport.js
+++ stackle_api/app/lib/passport.js
@@ -6,84 +6,84 @@ const UserModel = require('../models/user');
 
 //used by passport engine for serializing user
 passport.serializeUser(function(user, done) {
-  done(null, user);
+    done(null, user);
 });
 
 //used by passport engine for de-serializing user
 passport.deserializeUser(function(user, done) {
-  done(null, user);
+    done(null, user);
 });
 
 
 //Oauth strategy used by passportJS 
 //TODO - change authConfig in production env
-var strategy = new GithubStratergy({clientID: authConfig.clientId, clientSecret: authConfig.secret, callbackURL: authConfig.callback},
-             function(token, refreshToken, profile, cb){
+var strategy = new GithubStratergy({
+        clientID: authConfig.clientId,
+        clientSecret: authConfig.secret,
+        callbackURL: authConfig.callback
+    },
+    function(token, refreshToken, profile, cb) {
 
-              var user = profile;
-       				var data = user._json;
-        			var currentUser = {};
-        			
-        			currentUser.userId = user.username;
-        			currentUser.picUrl = data.avatar_url;
-        			currentUser.profileUrl = user.profileUrl;
-        			currentUser.token = token;        			
+        var user = profile;
+        var data = user._json;
+        var currentUser = {};
 
-        			if(data.email == null || !data.email)
-                  currentUser.email = 'N/A';
-        			else
-                  currentUser.email = data.email;
-	
-              if(user.displayName == null || !user.displayName || user.displayName === undefined)
-                  currentUser.name = currentUser.userId;
-              else
-                  currentUser.name = user.displayName;
-                            
-              // UserModel.findOrCreateUser(currentUser, cb);  
+        currentUser.userId = user.username;
+        currentUser.picUrl = data.avatar_url;
+        currentUser.profileUrl = user.profileUrl;
+        currentUser.token = token;
 
-              UserModel.findOne({userId: currentUser.userId}).exec(function(err, data){
-  
-                if(err){
-                  cb(err, false);
-                }
-  
-                else if(!data){
-                  var temp = new UserModel(currentUser);
-                  temp.save();
-                  cb(null, temp);
-                }
-  
-                else{
-                  data.token = token;
-                  data.save();
-                  cb(null, data);
-                } 
+        if (data.email == null || !data.email)
+            currentUser.email = 'N/A';
+        else
+            currentUser.email = data.email;
 
-              });
-         });
+        if (user.displayName == null || !user.displayName || user.displayName === undefined)
+            currentUser.name = currentUser.userId;
+        else
+            currentUser.name = user.displayName;
+
+        // UserModel.findOrCreateUser(currentUser, cb);  
+
+        UserModel.findOne({
+            userId: currentUser.userId
+        }).exec(function(err, data) {
+
+            if (err) {
+                cb(err, false);
+            } else if (!data) {
+                var temp = new UserModel(currentUser);
+                temp.save();
+                cb(null, temp);
+            } else {
+                data.token = token;
+                data.save();
+                cb(null, data);
+            }
+
+        });
+    });
 
 //Validating Token 
 var strategyToken = new BearerStratergy(
-        function(token, done) {
-            UserModel.findOne({ token: token },
-                function(err, user) {
-                    if(err || !user) {
-                        done(err, false);
-                    }
-                    else{
-                       done(null, user);
-                    }
-                    
+    function(token, done) {
+        UserModel.findOne({
+                token: token
+            },
+            function(err, user) {
+                if (err || !user) {
+                    done(err, false);
+                } else {
+                    done(null, user);
                 }
-            );
-        }
-    );
+
+            }
+        );
+    }
+);
 
 passport.use(strategy);
 passport.use(strategyToken);
 
 
 module.exports = passport;
-
-
-
