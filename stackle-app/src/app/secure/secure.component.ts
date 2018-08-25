03f9c5727a29af4778d968276446b67505be6a2d
VibhorCodecianGupta <VibhorCodecianGupta@users.noreply.github.com>
2018-08-25 19:26:25 +0530

    profile avatar

--- stackle-app/src/app/secure/secure.component.ts
+++ stackle-app/src/app/secure/secure.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 
 import { AuthService } from '../services/auth.service';
 import { StackService } from '../services/stack.service';
+import { ProfileService } from '../services/profile.service';
 
 @Component({
   selector: 'app-secure',
@@ -11,20 +12,32 @@ import { StackService } from '../services/stack.service';
 })
 export class SecureComponent implements OnInit {
 
+private profileObj:any;
+
   constructor(
     private auth: AuthService,
     private stackService: StackService,
+    private profileService: ProfileService,
     private router: Router
   ) {
     auth.handleAuthentication();
    }
 
   ngOnInit() {
-    this.stackService.getAllOrgs().subscribe((response)=> {
-
+    this.stackService.getAllOrgs().subscribe((response)=> {});
+    this.profileObj = {};
+    this.getProfileObj();
+  }
+  
+  getProfileObj() {
+    let username = 'ntbandara3'
+    this.profileService.getProfileObject(username).subscribe(response => {
+      this.profileObj = JSON.parse(response['_body']);
+    }, error => {
+        console.error(error)
     });
   }
-
+  
   public navigateToCreatePost() {
     this.router.navigate(['app/createPost']);
   }
