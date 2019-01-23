f2cb012c2c90216de825602267db96eacc39fd98
YashMeh <mehrotra.yash9@gmail.com>
2019-01-23 14:40:28 +0530

    Fixed Depreciation warnings for mongodb version>3.5

--- stackle_api/config/database.js
+++ stackle_api/config/database.js
@@ -1,13 +1,21 @@
 module.exports = {
-    url: process.env.LOCAL_DB || "mongodb://localhost/main",
-    testurl: "mongodb://localhost/node-test",
+    url: process.env.LOCAL_DB || "mongodb://localhost:27017/main",
+    testurl: "mongodb://localhost:27017/node-test",
     alturl: "mongodb://ordinary:H3ll0w0rld@ds149201.mlab.com:49201/stackle",
     option: function(version) {
         var opt;
         if (version < '5') {
             opt = {
                 useMongoClient: true
+                 
             };
+        }
+        else
+        {
+            opt=
+            {
+                useNewUrlParser: true
+            }
         }
         return opt;
     }
